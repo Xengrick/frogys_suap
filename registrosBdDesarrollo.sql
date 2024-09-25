19c2ad455b61f1e328dc2e573def355b322a4495
Xengrick <xengrick99@gmail.com>
2024-09-24 17:11:54 -0700

    Base de Datos
    Agregado usuarios administrador

--- registrosBdDesarrollo.sql
+++ registrosBdDesarrollo.sql
@@ -21,4 +21,18 @@ INSERT INTO examen.asignacion (idProfesor, idUnidadAprendizaje) VALUES
 (1,4),
 (1,5);
 
+-- Tabla Usuario  
+CREATE TABLE Usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    rol VARCHAR(20) NOT NULL CHECK (rol IN ('ADMIN', 'PROFESOR'))
+);
 
+ALTER TABLE Usuario
+CHANGE COLUMN username nombreUsuario VARCHAR(50) NOT NULL,
+CHANGE COLUMN password clave VARCHAR(100) NOT NULL;
+
+INSERT INTO examen.usuario (idUsuario, nombreUsuario, clave, rol) VALUES
+(1,'admin1',1234,'ADMIN'),
+(2,'admin2',5678,'ADMIN');
